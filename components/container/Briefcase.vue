d90c569a3f276ddc58337128f71bf1416ec66b4e
MoY8462 <moy.sav21@hotmail.com>
2023-08-31 22:46:49 -0600

    build: :construction: Card in progress hidden

--- components/container/Briefcase.vue
+++ components/container/Briefcase.vue
@@ -2,12 +2,12 @@
   <div>
     <h1 class="text-green-dark text-2xl font-semibold">Portafolio</h1>
     <h3 class="text-gray text-base font-semibold">Proyectos recientes</h3>
-    <section v-for="option in options">
+    <!-- <section v-for="option in options">
       <BaseCard 
         :url="option.url"
         :title="option.title"
         :description="option.description" />
-    </section>
+    </section> -->
   </div>
 </template>
 <script setup>
