29866b3854f45aee4ab36f579cc37e74272de6e6
MoY8462 <moy.sav21@hotmail.com>
2023-08-31 22:46:15 -0600

    build: :construction: Card in progress

--- components/base/Card.vue
+++ components/base/Card.vue
@@ -2,13 +2,13 @@
     <div>
         <img :src="url" :alt="url">
         <h2>{{ title }}</h2>
-        <h3>{{ subtitle }}</h3>
+        <h3>{{ description }}</h3>
     </div>
 </template>
 <script setup>
 defineProps({
     url: String,
     title: String,
-    subtitle: String
+    description: String
 })
 </script>
--- components/container/Briefcase.vue
+++ components/container/Briefcase.vue
@@ -2,6 +2,23 @@
   <div>
     <h1 class="text-green-dark text-2xl font-semibold">Portafolio</h1>
     <h3 class="text-gray text-base font-semibold">Proyectos recientes</h3>
-    <BaseCard></BaseCard>
+    <section v-for="option in options">
+      <BaseCard 
+        :url="option.url"
+        :title="option.title"
+        :description="option.description" />
+    </section>
   </div>
 </template>
+<script setup>
+import { ref } from "vue"
+
+const options = ref([
+  {
+    url: "./public/code.png",
+    title: "Nuxt 2 con SuperMap",
+    description: "Geoportal elaborado con SuperMap iClient para Vue.js"
+  }
+])
+
+</script>
